57fff82b296b1aeca981bdc99b02d9edd7b180bb
Mutiara Azzahrani Artama <102508852+MutiaraArtama@users.noreply.github.com>
2022-12-01 13:12:29 +0700

    Add files via upload

--- MonageApp/app/src/main/java/com/example/monageapp/Transaction.kt
+++ MonageApp/app/src/main/java/com/example/monageapp/Transaction.kt
@@ -3,10 +3,7 @@ package com.example.monageapp
 import java.io.Serializable
 
 class Transaction {
-    val id: Int,
     val tanggal: String? = null,
     val tittle: String? = null,
     val amount: Double,
-    val description: String? = null,
-    )
 }
--- MonageApp/app/src/main/java/com/example/monageapp/TransactionAdapter.kt
+++ MonageApp/app/src/main/java/com/example/monageapp/TransactionAdapter.kt
@@ -1,12 +1,57 @@
 package com.example.monageapp
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 
-class modelAdapter {
+class modelAdapter(private var transactions: List<Transaction>) :
+        RecyclerView.Adapter<modelAdapter.TransactionHolder>() {
 
-}
+        class TransactionHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val tittle : TextView = view.findViewById(R.id.txt_title)
+            val amount : TextView = view.findViewById(R.id.amount)
+            val tanggal :  TextView = view.findViewById(R.id.tanggal)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
+            return TransactionHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: TransactionHolder, position: Int) {
+            val transaction = transactions[position]
+            val context = holder.amount.context
+
+            if(transaction.amount >= 0){
+                holder.amount.text = "+ $%.2f".format(transaction.amount)
+                holder.amount.setTextColor(ContextCompat.getColor(context, R.color.green))
+            }else {
+                holder.amount.text = "- $%.2f".format(Math.abs(transaction.amount))
+                holder.amount.setTextColor(ContextCompat.getColor(context, R.color.red))
+            }
+
+            holder.tittle.text = transaction.tittle
+            holder.tanggal.text = transaction.tanggal
+
+            holder.itemView.setOnClickListener {
+                val intent = Intent(context, DetailedActivity::class.java)
+                intent.putExtra("transaction", transaction)
+                context.startActivity(intent)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return transactions.size
+        }
+
+        fun setData(transactions: List<Transaction>){
+            this.transactions = transactions
+            notifyDataSetChanged()
+        }
+    }
